af0f1994eed6c492422be79acd09ebb78e92eb45
Thiago_Cruz <101190863+mistickesterio@users.noreply.github.com>
2022-09-14 19:33:49 -0300

    otimizado 100%

--- j.s/JavaScript.js
+++ j.s/JavaScript.js
@@ -18,93 +18,41 @@ function criarInputs() {
     //Maior e menor Número
 
     do {
-
-      //Maior numero
-  
       for (var i = 0; i < 1; i++) {
         alert(`Maior numero = ${Math.max(vetor[0],vetor[1],vetor[2],vetor[3],vetor[4])}`);
-      }
-
-      //Menor numero
-
-      for (var i = 0; i < 1; i++) {
         alert(`Menor numero = ${Math.min(vetor[0],vetor[1],vetor[2],vetor[3],vetor[4])}`);
-      }
       
-    } while (i < 1);
-  
-
     //Média
 
-    for (var i = 0; i < 1; i++) {
       alert("Media = " + (vetor[0]+vetor[1]+vetor[2]+vetor[3]+vetor[4])/5);
-    }
-
+    
      //Raiz
 
-     for (var i = 0; i < 1; i++) {
       alert(`Raiz input1 = ${Math.sqrt(vetor[0])}`);
-    }
-
-    for (var i = 0; i < 1; i++) {
       alert(`Raiz input2 = ${Math.sqrt(vetor[1])}`);
-    }
-
-    for (var i = 0; i < 1; i++) {
       alert(`Raiz input3 = ${Math.sqrt(vetor[2])}`);
-    }
-
-    for (var i = 0; i < 1; i++) {
       alert(`Raiz input4 = ${Math.sqrt(vetor[3])}`);
-    }
-
-    for (var i = 0; i < 1; i++) {
       alert(`Raiz input5 = ${Math.sqrt(vetor[4])}`);
-    }
-
+    
     //Potência
 
-    for (var i = 0; i < 1; i++) {
       alert(`Potencia input1 = ${Math.pow(vetor[0], vetor[0] )}`);
-    }
-
-    for (var i = 0; i < 1; i++) {
       alert(`Potencia input2 = ${Math.pow(vetor[1], vetor[1])}`);
-    }
-
-    for (var i = 0; i < 1; i++) {
       alert(`Potencia input3 = ${Math.pow(vetor[2], vetor[2])}`);
-    }
-
-    for (var i = 0; i < 1; i++) {
       alert(`Potencia input4 = ${Math.pow(vetor[3], vetor[3])}`);
-    }
-
-    for (var i = 0; i < 1; i++) {
       alert(`Potencia input5 = ${Math.pow(vetor[4], vetor[4])}`);
     }
-
 
     //Tabuada
 
     for (var i = 0; i <= 10; i++) {
       divareaDeInputs.innerHTML += `<p>${vetor[0]} x ${i} = ${vetor[0]*i}</p>`;
-    }
-
-    for (var i = 0; i <= 10; i++) {
       divareaDeInputs.innerHTML += `<p>${vetor[1]} x ${i} = ${vetor[1]*i}</p>`;
-    }
-
-    for (var i = 0; i <= 10; i++) {
       divareaDeInputs.innerHTML += `<p>${vetor[2]} x ${i} = ${vetor[2]*i}</p>`;
-    }
-
-    for (var i = 0; i <= 10; i++) {
       divareaDeInputs.innerHTML += `<p>${vetor[3]} x ${i} = ${vetor[3]*i}</p>`;
-    }
-
-    for (var i = 0; i <= 10; i++) {
       divareaDeInputs.innerHTML += `<p>${vetor[4]} x ${i} = ${vetor[4]*i}</p>`;
     }
+
+  } while (i < 1);
 
   }
